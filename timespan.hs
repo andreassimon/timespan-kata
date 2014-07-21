ea267a53124a61bd2a238c43c333aa6079a56885
Andreas Simon <coding@andreas-simon.info>
2014-07-21 22:44:46 +0200

    Step 2.2
    Extend your function to support minutes e.g.
    prettyPrint(140) => "2 minutes, 20 seconds".
    Don't show minutes, unless the amount of seconds is large enough
    (e.g. prettyPrint(40) => "40 seconds") => Dont break the API (yet).

--- timespan.hs
+++ timespan.hs
@@ -6,11 +6,23 @@ import Test.Framework
 import Test.Framework.TestManager
 
 prettyPrint :: Int -> String
-prettyPrint seconds = (show seconds) ++ " seconds"
+prettyPrint seconds = _prettyPrint seconds 0
+
+_prettyPrint :: Int -> Int -> String
+_prettyPrint seconds 0
+  | seconds < 60   =   (show seconds) ++ " seconds"
+  | otherwise      =   _prettyPrint (seconds - 60) (1)
+_prettyPrint seconds minutes
+  | seconds < 60   =   (show minutes) ++ " minutes, " ++ (show seconds) ++ " seconds"
+  | otherwise      =   _prettyPrint (seconds - 60) (minutes + 1)
 
 test_1_1 =
     do assertEqual "30 seconds" (prettyPrint 30)
        assertEqual "20 seconds" (prettyPrint 20)
+
+test_1_2 =
+    do assertEqual "2 minutes, 20 seconds" (prettyPrint 140)
+       assertEqual "3 minutes, 0 seconds" (prettyPrint 180)
 
 main =
     do args <- getArgs
