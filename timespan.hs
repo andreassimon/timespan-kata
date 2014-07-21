48f9ec4e8b0dc56eaa59cc84725c840e84bace51
Andreas Simon <coding@andreas-simon.info>
2014-07-21 22:34:05 +0200

    Step 1.1
    Implement a function that, given a number of seconds, prints the seconds suffixed with " seconds". E.g. prettyPrint(30) => "30 seconds"

--- timespan.hs
+++ timespan.hs
@@ -5,10 +5,12 @@ import System.Environment
 import Test.Framework
 import Test.Framework.TestManager
 
-test_nonEmpty = 
-    do assertEqual [1] [1]
+prettyPrint :: Int -> String
+prettyPrint seconds = (show seconds) ++ " seconds"
 
-test_empty = assertEqual ([] :: [Int]) ([] :: [Int])
+test_1_1 =
+    do assertEqual "30 seconds" (prettyPrint 30)
+       assertEqual "20 seconds" (prettyPrint 20)
 
 main =
     do args <- getArgs
