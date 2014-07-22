e5e3cb502a5569823934954c2ace8775922fffdf
Andreas Simon <coding@andreas-simon.info>
2014-07-22 13:55:42 +0200

    Step 3.3
    Use the words "over" and "almost" to hint the rounding to the user.
    E.g. prettyPrint(198719, lossy: true) => "over 2 days"
    and prettyPrint(233280, lossy: true) => "almost 3 days"

--- timespan-list.hs
+++ timespan-list.hs
@@ -7,61 +7,61 @@ import Test.Framework.TestManager
 
 
 test_1_1 =
-    do assertEqual "30 seconds" (prettyPrint $ simplify (Seconds 30))
-       assertEqual "20 seconds" (prettyPrint $ simplify (Seconds 20))
+    do assertEqual "30 seconds" (prettyPrint $ Main.Just $ simplify (Seconds 30))
+       assertEqual "20 seconds" (prettyPrint $ Main.Just $ simplify (Seconds 20))
 
 test_1_2 =
-    do assertEqual "2 minutes, 20 seconds" (prettyPrint $ simplify (Seconds 140))
-       assertEqual "3 minutes, 0 seconds" (prettyPrint $ simplify (Seconds 180))
+    do assertEqual "2 minutes, 20 seconds" (prettyPrint $ Main.Just $ simplify (Seconds 140))
+       assertEqual "3 minutes, 0 seconds" (prettyPrint $ Main.Just $ simplify (Seconds 180))
 
 -- -- Implement support for hours (days, months and years (we assume 30 days = 1 month, 1 year = 12 months))
 test_1_3_hours =
-    do assertEqual "2 hours, 2 minutes, 2 seconds" (prettyPrint $ simplify $ (Seconds (2*60*60 + 2*60 + 2)))
+    do assertEqual "2 hours, 2 minutes, 2 seconds" (prettyPrint $ Main.Just $ simplify $ (Seconds (2*60*60 + 2*60 + 2)))
 
 test_1_3_days =
-    do assertEqual "2 days, 2 hours, 2 minutes, 2 seconds" (prettyPrint $ simplify $ (Seconds (2*24*60*60 + 2*60*60 + 2*60 + 2)))
+    do assertEqual "2 days, 2 hours, 2 minutes, 2 seconds" (prettyPrint $ Main.Just $ simplify $ (Seconds (2*24*60*60 + 2*60*60 + 2*60 + 2)))
 
 test_1_3_months =
-    do assertEqual "2 months, 2 days, 2 hours, 2 minutes, 2 seconds" (prettyPrint $ simplify $ (Seconds (2*30*24*60*60 + 2*24*60*60 + 2*60*60 + 2*60 + 2)))
+    do assertEqual "2 months, 2 days, 2 hours, 2 minutes, 2 seconds" (prettyPrint $ Main.Just $ simplify $ (Seconds (2*30*24*60*60 + 2*24*60*60 + 2*60*60 + 2*60 + 2)))
 
 test_1_3_years =
-    do assertEqual "2 years, 2 months, 2 days, 2 hours, 2 minutes, 2 seconds" (prettyPrint $ simplify $ (Seconds (2*12*30*24*60*60 + 2*30*24*60*60 + 2*24*60*60 + 2*60*60 + 2*60 + 2)))
+    do assertEqual "2 years, 2 months, 2 days, 2 hours, 2 minutes, 2 seconds" (prettyPrint $ Main.Just $ simplify $ (Seconds (2*12*30*24*60*60 + 2*30*24*60*60 + 2*24*60*60 + 2*60*60 + 2*60 + 2)))
 
 test_2_1 =
      do assertEqual "1 second" (prettyPrintBriefly $ simplify (Seconds 1))
         assertEqual "1 day, 1 hour" (prettyPrintBriefly $ simplify (Seconds 90012))
      where
-        prettyPrintBriefly ts = prettyPrint $ filter timespanNotZero $ take 3 ts
+        prettyPrintBriefly ts = prettyPrint $ Main.Just $ filter timespanNotZero $ take 3 ts
 
 test_2_2 =
      do assertEqual "2 days, 0 hours, 1 minute" (prettyPrintBriefly $ simplify (Seconds 172860))
      where
-        prettyPrintBriefly ts = prettyPrint $ take 3 ts
+        prettyPrintBriefly ts = prettyPrint $ Main.Just $ take 3 ts
 
 test_2_3 =
      do assertEqual "2 days, 0 hours, 1 minute" (prettyPrintBriefly $ simplify (Seconds 172860))
      where
-        prettyPrintBriefly ts = prettyPrint $ take 3 ts
+        prettyPrintBriefly ts = prettyPrint $ Main.Just $ take 3 ts
 
 test_3_1 =
      do assertEqual "2 days" (prettyPrintLossy $ simplify (Seconds 172862))
      where
-        prettyPrintLossy ts = prettyPrint $ take 1 ts
+        prettyPrintLossy ts = prettyPrint $ Main.Just $ take 1 ts
 
 test_3_2 =
-     do assertEqual "1 second" (prettyPrintLossy $ simplify (Seconds (1*second)))
-        assertEqual "2 seconds" (prettyPrintLossy $ simplify (Seconds (2*seconds)))
-        assertEqual "1 minute" (prettyPrintLossy $ simplify (Seconds (1*minute + 29*seconds)))
-        assertEqual "2 minutes" (prettyPrintLossy $ simplify (Seconds (1*minute + 30*seconds)))
-        assertEqual "1 hour" (prettyPrintLossy $ simplify (Seconds (1*hour + 29*minutes)))
-        assertEqual "2 hours" (prettyPrintLossy $ simplify (Seconds (1*hour + 30*minutes)))
-        assertEqual "2 days" (prettyPrintLossy $ simplify (Seconds 198719))
-        assertEqual "3 days" (prettyPrintLossy $ simplify (Seconds 233280))
-        assertEqual "1 month" (prettyPrintLossy $ simplify (Seconds (1*month + 14*days)))
-        assertEqual "2 months" (prettyPrintLossy $ simplify (Seconds (1*month + 15*days)))
-        assertEqual "1 year" (prettyPrintLossy $ simplify (Seconds (1*year + 5*months)))
-        assertEqual "2 years" (prettyPrintLossy $ simplify (Seconds (1*year + 6*months)))
-        assertEqual "2 years, 1 month" (prettyPrint $ Main.round $ take 3 $ simplify (Seconds (2*years + 1*month + 14*days)))
+     do assertEqual "1 second" (prettyPrintLossy 2 $ simplify (Seconds (1*second)))
+        assertEqual "2 seconds" (prettyPrintLossy 2 $ simplify (Seconds (2*seconds)))
+        assertEqual "over 1 minute" (prettyPrintLossy 2 $ simplify (Seconds (1*minute + 29*seconds)))
+        assertEqual "almost 2 minutes" (prettyPrintLossy 2 $ simplify (Seconds (1*minute + 30*seconds)))
+        assertEqual "over 1 hour" (prettyPrintLossy 2 $ simplify (Seconds (1*hour + 29*minutes)))
+        assertEqual "almost 2 hours" (prettyPrintLossy 2 $ simplify (Seconds (1*hour + 30*minutes)))
+        assertEqual "over 2 days" (prettyPrintLossy 2 $ simplify (Seconds 198719))
+        assertEqual "almost 3 days" (prettyPrintLossy 2 $ simplify (Seconds 233280))
+        assertEqual "over 1 month" (prettyPrintLossy 2 $  simplify (Seconds (1*month + 14*days)))
+        assertEqual "almost 2 months" (prettyPrintLossy 2 $ simplify (Seconds (1*month + 15*days)))
+        assertEqual "over 1 year" (prettyPrintLossy 2 $ simplify (Seconds (1*year + 5*months)))
+        assertEqual "almost 2 years" (prettyPrintLossy 2 $ simplify (Seconds (1*year + 6*months)))
+        assertEqual "over 2 years, 1 month" (prettyPrintLossy 3 $ simplify (Seconds (2*years + 1*month + 14*days)))
      where
         year = years
         years = 12*months
@@ -74,7 +74,20 @@ test_3_2 =
         minutes = 60*seconds
         second = seconds
         seconds = 1
+        prettyPrintLossy n ts = prettyPrint $ Main.round $ take n ts
+
+test_3_3_a =
+     do assertEqual (Main.Floor [(Days 2)]) (Main.round $ take 2 $ simplify (Seconds (198719)))
+        assertEqual "over 2 years, 1 month"  (prettyPrint (Main.Floor ([(Years 2), (Months 1)])))
+        assertEqual "over 2 days, 10 hours"  (prettyPrint (Main.Floor ([(Days 2), (Hours 10)])))
+        assertEqual "over 2 years"  (prettyPrint (Main.Floor ([(Years 2)])))
+        assertEqual "over 2 days"  (prettyPrint (Main.Floor ([(Days 2)])))
+test_3_3 =
+     do assertEqual "over 2 days" (prettyPrintLossy $ simplify (Seconds (198719)))
+        assertEqual "almost 3 days" (prettyPrintLossy $ simplify (Seconds (233280)))
+     where
         prettyPrintLossy ts = prettyPrint $ Main.round $ take 2 ts
+
 
 data Timespan = Seconds Integer |
                 Minutes Integer |
@@ -104,8 +117,10 @@ timespanNotZero (Months 0)  = False
 timespanNotZero (Years 0)   = False
 timespanNotZero _           = True
 
-prettyPrint :: [Timespan] -> String
-prettyPrint ts = foldl1 (\x y -> concat [x,", ",y]) $ map show ts
+prettyPrint :: RoundedTimespan -> String
+prettyPrint (Main.Just ts) = foldl1 (\x y -> concat [x,", ",y]) $ map show ts
+prettyPrint (Main.Floor ts) = "over " ++ prettyPrint (Main.Just ts)
+prettyPrint (Main.Ceiling ts) = "almost " ++ prettyPrint (Main.Just ts)
 
 simplify :: Timespan -> [Timespan]
 simplify (Seconds seconds) = simplifyL [Seconds seconds]
@@ -128,24 +143,30 @@ simplifyL [(Seconds seconds)]
   | otherwise    = simplifyL [(Minutes (seconds `div` 60)), (Seconds (seconds `mod` 60))]
 simplifyL ts     = ts
 
-round :: [Timespan] -> [Timespan]
+data RoundedTimespan = Floor [Timespan] |
+                       Ceiling [Timespan] |
+                       Just [Timespan] deriving (Eq, Show)
+
+round :: [Timespan] -> RoundedTimespan
 round [(Years y), (Months m)]
-  | m < 6 = [(Years y)]
-  | otherwise = [(Years (y+1))]
+  | m < 6 = Floor [(Years y)]
+  | otherwise = Ceiling [(Years (y+1))]
 round [(Months m), (Days d)]
-  | d < 15 = [(Months m)]
-  | otherwise = [(Months (m+1))]
+  | d < 15 = Floor [(Months m)]
+  | otherwise = Ceiling [(Months (m+1))]
 round [(Days d), (Hours h)]
-  | h < 12 = [(Days d)]
-  | otherwise = [(Days (d+1))]
+  | h < 12 = Floor [(Days d)]
+  | otherwise = Ceiling [(Days (d+1))]
 round [(Hours h), (Minutes m)]
-  | m < 30 = [(Hours h)]
-  | otherwise = [(Hours (h+1))]
+  | m < 30 = Floor [(Hours h)]
+  | otherwise = Ceiling [(Hours (h+1))]
 round [(Minutes m), (Seconds s)]
-  | s < 30 = [(Minutes m)]
-  | otherwise = [(Minutes (m+1))]
-round [(Seconds s)] = [(Seconds s)]
-round (t:ts) = (t:(Main.round ts))
+  | s < 30 = Floor [(Minutes m)]
+  | otherwise = Ceiling [(Minutes (m+1))]
+round [(Seconds s)] = Main.Just [(Seconds s)]
+round (t:ts) = (cc t (Main.round ts))
+  where cc t (Floor ts) = Floor (t:ts)
+        cc t (Ceiling ts) = Ceiling (t:ts)
 
 
 main =
