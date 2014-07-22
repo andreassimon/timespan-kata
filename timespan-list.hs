eb8a41e7973f55dcbb7e846a3e5d9133f7a770ad
Andreas Simon <coding@andreas-simon.info>
2014-07-22 11:37:29 +0200

    Extended new attempt for months and years

--- timespan-list.hs
+++ timespan-list.hs
@@ -24,8 +24,8 @@ test_1_3_days =
 test_1_3_months =
     do assertEqual "2 months, 2 days, 2 hours, 2 minutes, 2 seconds" (prettyPrint $ simplify $ (Seconds (2*30*24*60*60 + 2*24*60*60 + 2*60*60 + 2*60 + 2)))
 
--- test_1_3_years =
---     do assertEqual "2 years, 2 months, 2 days, 2 hours, 2 minutes, 2 seconds" (prettyPrint LongForm $ 2*12*30*24*60*60 + 2*30*24*60*60 + 2*24*60*60 + 2*60*60 + 2*60 + 2)
+test_1_3_years =
+    do assertEqual "2 years, 2 months, 2 days, 2 hours, 2 minutes, 2 seconds" (prettyPrint $ simplify $ (Seconds (2*12*30*24*60*60 + 2*30*24*60*60 + 2*24*60*60 + 2*60*60 + 2*60 + 2)))
 
 -- test_1_4 =
 --      do assertEqual "1 second" (prettyPrint BriefForm $ 1)
@@ -34,7 +34,9 @@ test_1_3_months =
 data Timespan = Seconds Integer |
                 Minutes Integer |
                 Hours Integer   |
-                Days Integer deriving Eq
+                Days Integer    |
+                Months Integer  |
+                Years Integer deriving Eq
 
 showTimespan :: String -> Integer -> String
 showTimespan w 1 = "1 " ++ w
@@ -45,6 +47,8 @@ instance Show Timespan where
   show (Minutes m) = showTimespan "minute" m
   show (Hours h)   = showTimespan "hour" h
   show (Days d)    = showTimespan "day" d
+  show (Months m)  = showTimespan "month" m
+  show (Years y)   = showTimespan "year" y
 
 prettyPrint :: [Timespan] -> String
 prettyPrint ts = foldl1 (\x y -> concat [x,", ",y]) $ map show ts
@@ -53,15 +57,22 @@ simplify :: Timespan -> [Timespan]
 simplify (Seconds seconds) = simplifyL [Seconds seconds]
 
 simplifyL :: [Timespan] -> [Timespan]
+simplifyL ((Months months):ts)
+  | months < 12  = (Months months):ts
+  | otherwise    = simplifyL $ (Years (months `div` 12)):(Months (months `mod` 12)):ts
+simplifyL ((Days days):ts)
+  | days < 30    = (Days days):ts
+  | otherwise    = simplifyL $ (Months (days `div` 30)):(Days (days `mod` 30)):ts
 simplifyL ((Hours hours):ts)
   | hours < 24   = (Hours hours):ts
-  | otherwise    = (Days (hours `div` 24)):(Hours (hours `mod` 24)):ts
+  | otherwise    = simplifyL $ (Days (hours `div` 24)):(Hours (hours `mod` 24)):ts
 simplifyL ((Minutes minutes):ts)
   | minutes < 60 = (Minutes minutes):ts
   | otherwise    = simplifyL $ (Hours (minutes `div` 60)):(Minutes (minutes `mod` 60)):ts
 simplifyL [(Seconds seconds)]
   | seconds < 60 = [(Seconds seconds)]
   | otherwise    = simplifyL [(Minutes (seconds `div` 60)), (Seconds (seconds `mod` 60))]
+simplifyL ts     = ts
 
 
 main =
